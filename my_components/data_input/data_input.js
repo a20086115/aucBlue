3b2e5172c524cc9bdb105e33eaaf55f0faef6b63
Yu wenqiang <yuwenqiang@topscomm.com>
2019-11-25 09:19:44 +0800

    自定义输入组件

---
+++ my_components/data_input/data_input.js
@@ -0,0 +1,23 @@
+// my_components/data_input/data_input.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
